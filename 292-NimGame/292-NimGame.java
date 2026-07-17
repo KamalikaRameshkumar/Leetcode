2a798eab3a79e461da4fd606bfc023edb080469d
Kamalika <kamalikarameshkumar@gmail.com>
2026-07-17 14:59:29 +0530

    Create 292-NimGame

---
+++ 292-NimGame/292-NimGame.java
@@ -0,0 +1,13 @@
+// Last updated: 7/17/2026, 2:59:29 PM
+class Solution {
+    public boolean canWinNim(int n) {
+        if(n%4==0){
+            return false;
+        
+        }
+        else{
+            return true;
+        }
+        
+    }
+}
