7fd665676be1bd10013351fb289063646b26ddba
Kamalika <kamalikarameshkumar@gmail.com>
2026-07-17 15:00:17 +0530

    Create 136-SingleNumber

---
+++ 136-SingleNumber/136-SingleNumber.java
@@ -0,0 +1,18 @@
+// Last updated: 7/17/2026, 3:00:16 PM
+class Solution {
+    public int singleNumber(int[] nums) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        for (int num : nums) {
+            if (map.get(num) == 1)
+                return num;
+        }
+
+        return -1;
+    }
+}
