5be901bfdc66b58222f755ab48d6cf1ba5bf8106
Kamalika <kamalikarameshkumar@gmail.com>
2026-07-17 14:58:49 +0530

    Create 1005-UnivaluedBinaryTree

---
+++ 1005-UnivaluedBinaryTree/1005-UnivaluedBinaryTree.java
@@ -0,0 +1,30 @@
+// Last updated: 7/17/2026, 2:58:49 PM
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isUnivalTree(TreeNode root) {
+        return dfs(root, root.val);
+    }
+    private boolean dfs(TreeNode node, int value) {
+        if (node == null) {
+            return true;
+        }
+        if (node.val != value) {
+            return false;
+        }
+        return dfs(node.left, value) && dfs(node.right, value);
+    }
+}
