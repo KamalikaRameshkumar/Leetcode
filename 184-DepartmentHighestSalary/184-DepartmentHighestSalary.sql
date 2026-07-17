d7e7df3632bcf8632622b3433c6bb3a7fe78138c
Kamalika <kamalikarameshkumar@gmail.com>
2026-07-17 14:59:55 +0530

    Create 184-DepartmentHighestSalary

---
+++ 184-DepartmentHighestSalary/184-DepartmentHighestSalary.sql
@@ -0,0 +1,13 @@
+-- Last updated: 7/17/2026, 2:59:55 PM
+SELECT 
+    d.name AS Department,
+    e.name AS Employee,
+    e.salary AS Salary
+FROM Employee e
+JOIN Department d
+    ON e.departmentId = d.id
+WHERE e.salary = (
+    SELECT MAX(salary)
+    FROM Employee
+    WHERE departmentId = e.departmentId
+);
