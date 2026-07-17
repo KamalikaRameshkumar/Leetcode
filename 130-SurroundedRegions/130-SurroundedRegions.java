636f204ce544d54c84494a2aa3dfaf6315e16551
Kamalika <kamalikarameshkumar@gmail.com>
2026-07-17 15:00:19 +0530

    Create 130-SurroundedRegions

---
+++ 130-SurroundedRegions/130-SurroundedRegions.java
@@ -0,0 +1,45 @@
+// Last updated: 7/17/2026, 3:00:19 PM
+class Solution {
+    public void solve(char[][] board) {
+        int m = board.length;
+        int n = board[0].length;
+
+        // Step 1: Mark safe 'O's from borders
+        for (int i = 0; i < m; i++) {
+            if (board[i][0] == 'O') dfs(board, i, 0);
+            if (board[i][n - 1] == 'O') dfs(board, i, n - 1);
+        }
+
+        for (int j = 0; j < n; j++) {
+            if (board[0][j] == 'O') dfs(board, 0, j);
+            if (board[m - 1][j] == 'O') dfs(board, m - 1, j);
+        }
+
+        // Step 2: Flip and restore
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 'O') {
+                    board[i][j] = 'X'; // captured
+                } else if (board[i][j] == 'S') {
+                    board[i][j] = 'O'; // restore safe region
+                }
+            }
+        }
+    }
+
+    private void dfs(char[][] board, int i, int j) {
+        int m = board.length;
+        int n = board[0].length;
+
+        if (i < 0 || j < 0 || i >= m || j >= n || board[i][j] != 'O') {
+            return;
+        }
+
+        board[i][j] = 'S'; // mark as safe
+
+        dfs(board, i + 1, j);
+        dfs(board, i - 1, j);
+        dfs(board, i, j + 1);
+        dfs(board, i, j - 1);
+    }
+}
