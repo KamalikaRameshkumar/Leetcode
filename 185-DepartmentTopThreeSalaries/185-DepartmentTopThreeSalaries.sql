738c3080861ef671eac8b03cded331890bb10bf6
Kamalika <kamalikarameshkumar@gmail.com>
2026-07-17 14:59:54 +0530

    Create 185-DepartmentTopThreeSalaries

---
+++ 185-DepartmentTopThreeSalaries/185-DepartmentTopThreeSalaries.sql
@@ -0,0 +1,17 @@
+-- Last updated: 7/17/2026, 2:59:54 PM
+SELECT 
+    d.name AS Department,
+    e.name AS Employee,
+    e.salary AS Salary
+FROM (
+    SELECT 
+        e.*,
+        DENSE_RANK() OVER (
+            PARTITION BY e.departmentId 
+            ORDER BY e.salary DESC
+        ) AS rnk
+    FROM Employee e
+) e
+JOIN Department d
+    ON e.departmentId = d.id
+WHERE e.rnk <= 3;
