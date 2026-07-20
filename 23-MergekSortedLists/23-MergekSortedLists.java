9d7d6d446d186581dc9b4628285f40369727e779
Kamalika <kamalikarameshkumar@gmail.com>
2026-07-20 07:11:04 +0530

    File updated at 7/20/2026, 7:11:04 AM

--- 23-MergekSortedLists/23-MergekSortedLists.java
+++ 23-MergekSortedLists/23-MergekSortedLists.java
@@ -1,4 +1,4 @@
-// Last updated: 7/20/2026, 7:10:15 AM
+// Last updated: 7/20/2026, 7:11:04 AM
 1/**
 2 * Definition for singly-linked list.
 3 * public class ListNode {
@@ -9,31 +9,38 @@
 8 *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
 9 * }
 10 */
-11import java.util.PriorityQueue;
-12
-13class Solution {
-14    public ListNode mergeKLists(ListNode[] lists) {
-15        PriorityQueue<ListNode> pq = new PriorityQueue<>((a, b) -> a.val - b.val);
+11class Solution {
+12    public ListNode reverseKGroup(ListNode head, int k) {
+13        ListNode dummy = new ListNode(0);
+14        dummy.next = head;
+15        ListNode prevGroup = dummy;
 16
-17        for (ListNode node : lists) {
-18            if (node != null) {
-19                pq.offer(node);
-20            }
-21        }
+17        while (true) {
+18            ListNode kth = prevGroup;
+19            for (int i = 0; i < k && kth != null; i++) {
+20                kth = kth.next;
+21            }
 22
-23        ListNode dummy = new ListNode(0);
-24        ListNode current = dummy;
-25
-26        while (!pq.isEmpty()) {
-27            ListNode node = pq.poll();
-28            current.next = node;
-29            current = current.next;
+23            if (kth == null) {
+24                break;
+25            }
+26
+27            ListNode groupNext = kth.next;
+28            ListNode prev = groupNext;
+29            ListNode curr = prevGroup.next;
 30
-31            if (node.next != null) {
-32                pq.offer(node.next);
-33            }
-34        }
-35
-36        return dummy.next;
-37    }
-38}
+31            while (curr != groupNext) {
+32                ListNode temp = curr.next;
+33                curr.next = prev;
+34                prev = curr;
+35                curr = temp;
+36            }
+37
+38            ListNode temp = prevGroup.next;
+39            prevGroup.next = kth;
+40            prevGroup = temp;
+41        }
+42
+43        return dummy.next;
+44    }
+45}
