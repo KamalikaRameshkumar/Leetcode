d03c3b0966e52cdf3c068758c923c1e3d70abfa3
Kamalika <kamalikarameshkumar@gmail.com>
2026-07-21 09:37:59 +0530

    File updated at 7/21/2026, 9:37:58 AM

--- 75-SortColors/75-SortColors.java
+++ 75-SortColors/75-SortColors.java
@@ -1,31 +1,21 @@
-// Last updated: 7/21/2026, 9:37:06 AM
+// Last updated: 7/21/2026, 9:37:58 AM
 1class Solution {
-2    public void sortColors(int[] nums) {
-3
-4        int low = 0;
-5        int mid = 0;
-6        int high = nums.length - 1;
-7
-8        while (mid <= high) {
+2
+3    public List<List<Integer>> subsets(int[] nums) {
+4
+5        List<List<Integer>> result = new ArrayList<>();
+6        backtrack(0, nums, new ArrayList<>(), result);
+7        return result;
+8    }
 9
-10            if (nums[mid] == 0) {
-11                int temp = nums[low];
-12                nums[low] = nums[mid];
-13                nums[mid] = temp;
-14                low++;
-15                mid++;
-16            }
-17
-18            else if (nums[mid] == 1) {
-19                mid++;
-20            }
-21
-22            else {
-23                int temp = nums[mid];
-24                nums[mid] = nums[high];
-25                nums[high] = temp;
-26                high--;
-27            }
-28        }
-29    }
-30}
+10    public void backtrack(int index, int[] nums, List<Integer> current, List<List<Integer>> result) {
+11
+12        result.add(new ArrayList<>(current));
+13
+14        for (int i = index; i < nums.length; i++) {
+15            current.add(nums[i]);
+16            backtrack(i + 1, nums, current, result);
+17            current.remove(current.size() - 1);
+18        }
+19    }
+20}
