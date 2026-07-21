d57d794d8326a76c91ed34b51b998c29426a7315
Kamalika <kamalikarameshkumar@gmail.com>
2026-07-21 10:26:22 +0530

    File updated at 7/21/2026, 10:26:21 AM

--- 127-WordLadder/127-WordLadder.java
+++ 127-WordLadder/127-WordLadder.java
@@ -1,36 +1,19 @@
-// Last updated: 7/21/2026, 10:25:15 AM
+// Last updated: 7/21/2026, 10:26:21 AM
 1class Solution {
-2    public String multiply(String num1, String num2) {
+2    public int maxProfit(int[] prices) {
 3
-4        if (num1.equals("0") || num2.equals("0")) {
-5            return "0";
-6        }
-7
-8        int m = num1.length();
-9        int n = num2.length();
-10
-11        int[] result = new int[m + n];
-12
-13        for (int i = m - 1; i >= 0; i--) {
-14            for (int j = n - 1; j >= 0; j--) {
+4        int buy1 = Integer.MIN_VALUE;
+5        int sell1 = 0;
+6        int buy2 = Integer.MIN_VALUE;
+7        int sell2 = 0;
+8
+9        for (int price : prices) {
+10            buy1 = Math.max(buy1, -price);
+11            sell1 = Math.max(sell1, buy1 + price);
+12            buy2 = Math.max(buy2, sell1 - price);
+13            sell2 = Math.max(sell2, buy2 + price);
+14        }
 15
-16                int mul = (num1.charAt(i) - '0') * (num2.charAt(j) - '0');
-17
-18                int sum = mul + result[i + j + 1];
-19
-20                result[i + j + 1] = sum % 10;
-21                result[i + j] += sum / 10;
-22            }
-23        }
-24
-25        StringBuilder sb = new StringBuilder();
-26
-27        for (int num : result) {
-28            if (!(sb.length() == 0 && num == 0)) {
-29                sb.append(num);
-30            }
-31        }
-32
-33        return sb.toString();
-34    }
-35}
+16        return sell2;
+17    }
+18}
