7f8d75c04e7942bcf2114b01c17605dd6cf9249c
Kamalika <kamalikarameshkumar@gmail.com>
2026-07-21 09:49:34 +0530

    File updated at 7/21/2026, 9:49:33 AM

--- 17-LetterCombinationsofaPhoneNumber/17-LetterCombinationsofaPhoneNumber.java
+++ 17-LetterCombinationsofaPhoneNumber/17-LetterCombinationsofaPhoneNumber.java
@@ -1,34 +1,27 @@
-// Last updated: 7/21/2026, 9:47:39 AM
+// Last updated: 7/21/2026, 9:49:33 AM
 1class Solution {
-2    public int divide(int dividend, int divisor) {
+2    public int longestValidParentheses(String s) {
 3
-4        if (dividend == Integer.MIN_VALUE && divisor == -1) {
-5            return Integer.MAX_VALUE;
-6        }
-7
-8        long dvd = Math.abs((long) dividend);
-9        long dvs = Math.abs((long) divisor);
+4        Stack<Integer> stack = new Stack<>();
+5        stack.push(-1);
+6
+7        int max = 0;
+8
+9        for (int i = 0; i < s.length(); i++) {
 10
-11        int result = 0;
-12
-13        while (dvd >= dvs) {
-14
-15            long temp = dvs;
-16            int multiple = 1;
-17
-18            while (dvd >= (temp << 1)) {
-19                temp <<= 1;
-20                multiple <<= 1;
+11            if (s.charAt(i) == '(') {
+12                stack.push(i);
+13            } else {
+14                stack.pop();
+15
+16                if (stack.isEmpty()) {
+17                    stack.push(i);
+18                } else {
+19                    max = Math.max(max, i - stack.peek());
+20                }
 21            }
-22
-23            dvd -= temp;
-24            result += multiple;
-25        }
-26
-27        if ((dividend > 0) ^ (divisor > 0)) {
-28            return -result;
-29        }
-30
-31        return result;
-32    }
-33}
+22        }
+23
+24        return max;
+25    }
+26}
