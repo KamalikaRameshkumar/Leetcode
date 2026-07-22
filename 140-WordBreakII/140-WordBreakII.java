8514818f6a074be8ecb9ed838f7419ba520b4f4b
Kamalika <kamalikarameshkumar@gmail.com>
2026-07-22 15:13:19 +0530

    File updated at 7/22/2026, 3:13:19 PM

--- 140-WordBreakII/140-WordBreakII.java
+++ 140-WordBreakII/140-WordBreakII.java
@@ -1,34 +1,37 @@
-// Last updated: 7/22/2026, 3:11:49 PM
-1import java.util.*;
-2class Solution {
-3    public List<String> wordBreak(String s, List<String> wordDict) {
-4        Set<String> set = new HashSet<>(wordDict);
-5        Map<String, List<String>> memo = new HashMap<>();
-6        return dfs(s, set, memo);
-7    }
-8    private List<String> dfs(String s, Set<String> set, Map<String, List<String>> memo) {
-9        if (memo.containsKey(s)) {
-10            return memo.get(s);
-11        }
-12        List<String> result = new ArrayList<>();
-13        if (s.length() == 0) {
-14            result.add("");
-15            return result;
-16        }
-17        for (String word : set) {
-18            if (s.startsWith(word)) {
-19                List<String> subList = dfs(s.substring(word.length()), set, memo);
-20                for (String sub : subList) {
-21                    if (sub.equals("")) {
-22                        result.add(word);
-23                    } else {
-24                        result.add(word + " " + sub);
-25                    }
-26                }
-27            }
-28        }
-29
-30        memo.put(s, result);
-31        return result;
-32    }
-33}
+// Last updated: 7/22/2026, 3:13:19 PM
+1/*
+2// Definition for a Node.
+3class Node {
+4    int val;
+5    Node next;
+6    Node random;
+7
+8    public Node(int val) {
+9        this.val = val;
+10        this.next = null;
+11        this.random = null;
+12    }
+13}
+14*/
+15import java.util.HashMap;
+16class Solution {
+17    public Node copyRandomList(Node head) {
+18        if (head == null) {
+19            return null;
+20        }
+21        HashMap<Node, Node> map = new HashMap<>();
+22        Node curr = head;
+23        while (curr != null) {
+24            map.put(curr, new Node(curr.val));
+25            curr = curr.next;
+26        }
+27        curr = head;
+28        while (curr != null) {
+29            map.get(curr).next = map.get(curr.next);
+30            map.get(curr).random = map.get(curr.random);
+31            curr = curr.next;
+32        }
+33
+34        return map.get(head);
+35    }
+36}
