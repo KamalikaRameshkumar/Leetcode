680c529174981c6688158eb642f4b9093a906ce5
Kamalika <kamalikarameshkumar@gmail.com>
2026-07-22 15:19:33 +0530

    File updated at 7/22/2026, 3:19:33 PM

--- 49-GroupAnagrams/49-GroupAnagrams.java
+++ 49-GroupAnagrams/49-GroupAnagrams.java
@@ -1,22 +1,36 @@
-// Last updated: 7/22/2026, 3:16:52 PM
-1import java.util.*;
-2
-3class Solution {
-4    public List<List<String>> groupAnagrams(String[] strs) {
-5        HashMap<String, List<String>> map = new HashMap<>();
-6
-7        for (String str : strs) {
-8            char[] ch = str.toCharArray();
-9            Arrays.sort(ch);
-10            String key = new String(ch);
-11
-12            if (!map.containsKey(key)) {
-13                map.put(key, new ArrayList<>());
-14            }
-15
-16            map.get(key).add(str);
-17        }
-18
-19        return new ArrayList<>(map.values());
-20    }
-21}
+// Last updated: 7/22/2026, 3:19:33 PM
+1/**
+2 * Definition for singly-linked list.
+3 * public class ListNode {
+4 *     int val;
+5 *     ListNode next;
+6 *     ListNode() {}
+7 *     ListNode(int val) { this.val = val; }
+8 *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+9 * }
+10 */
+11class Solution {
+12    public ListNode reverseBetween(ListNode head, int left, int right) {
+13        if (head == null || left == right) {
+14            return head;
+15        }
+16
+17        ListNode dummy = new ListNode(0);
+18        dummy.next = head;
+19
+20        ListNode prev = dummy;
+21        for (int i = 1; i < left; i++) {
+22            prev = prev.next;
+23        }
+24
+25        ListNode curr = prev.next;
+26        for (int i = 0; i < right - left; i++) {
+27            ListNode temp = curr.next;
+28            curr.next = temp.next;
+29            temp.next = prev.next;
+30            prev.next = temp;
+31        }
+32
+33        return dummy.next;
+34    }
+35}
