c71f240b571cfc08fc35d7b07c0a3c027c8d4342
Kamalika <kamalikarameshkumar@gmail.com>
2026-07-22 15:15:23 +0530

    File updated at 7/22/2026, 3:15:23 PM

--- 140-WordBreakII/140-WordBreakII.java
+++ 140-WordBreakII/140-WordBreakII.java
@@ -1,34 +1,41 @@
-// Last updated: 7/22/2026, 3:14:08 PM
-1import java.util.*;
-2class Solution {
-3    public List<String> wordBreak(String s, List<String> wordDict) {
-4        Set<String> set = new HashSet<>(wordDict);
-5        Map<String, List<String>> memo = new HashMap<>();
-6        return dfs(s, set, memo);
-7    }
-8    private List<String> dfs(String s, Set<String> set, Map<String, List<String>> memo) {
-9        if (memo.containsKey(s)) {
-10            return memo.get(s);
-11        }
-12        List<String> result = new ArrayList<>();
-13        if (s.length() == 0) {
-14            result.add("");
-15            return result;
+// Last updated: 7/22/2026, 3:15:23 PM
+1/**
+2 * Definition for singly-linked list.
+3 * class ListNode {
+4 *     int val;
+5 *     ListNode next;
+6 *     ListNode(int x) {
+7 *         val = x;
+8 *         next = null;
+9 *     }
+10 * }
+11 */
+12public class Solution {
+13    public ListNode detectCycle(ListNode head) {
+14        if (head == null || head.next == null) {
+15            return null;
 16        }
-17        for (String word : set) {
-18            if (s.startsWith(word)) {
-19                List<String> subList = dfs(s.substring(word.length()), set, memo);
-20                for (String sub : subList) {
-21                    if (sub.equals("")) {
-22                        result.add(word);
-23                    } else {
-24                        result.add(word + " " + sub);
-25                    }
-26                }
-27            }
-28        }
-29
-30        memo.put(s, result);
-31        return result;
-32    }
-33}
+17
+18        ListNode slow = head;
+19        ListNode fast = head;
+20
+21        // Detect cycle
+22        while (fast != null && fast.next != null) {
+23            slow = slow.next;
+24            fast = fast.next.next;
+25
+26            if (slow == fast) {
+27                ListNode start = head;
+28
+29                while (start != slow) {
+30                    start = start.next;
+31                    slow = slow.next;
+32                }
+33
+34                return start;
+35            }
+36        }
+37
+38        return null;
+39    }
+40}
