d6c4a997492d564d6784af5a93971aa67fc1d5d3
Kamalika <kamalikarameshkumar@gmail.com>
2026-07-31 10:11:15 +0530

    File updated at 7/31/2026, 10:11:15 AM

--- 174-DungeonGame/174-DungeonGame.java
+++ 174-DungeonGame/174-DungeonGame.java
@@ -1,28 +1,32 @@
-// Last updated: 7/31/2026, 10:08:17 AM
+// Last updated: 7/31/2026, 10:11:15 AM
 1class Solution {
-2    public int calculateMinimumHP(int[][] dungeon) {
-3        int m = dungeon.length;
-4        int n = dungeon[0].length;
-5
-6        int[][] dp = new int[m][n];
-7
-8        dp[m - 1][n - 1] = Math.max(1, 1 - dungeon[m - 1][n - 1]);
-9
-10        for (int i = m - 2; i >= 0; i--) {
-11            dp[i][n - 1] = Math.max(1, dp[i + 1][n - 1] - dungeon[i][n - 1]);
-12        }
-13
-14        for (int j = n - 2; j >= 0; j--) {
-15            dp[m - 1][j] = Math.max(1, dp[m - 1][j + 1] - dungeon[m - 1][j]);
-16        }
-17
-18        for (int i = m - 2; i >= 0; i--) {
-19            for (int j = n - 2; j >= 0; j--) {
-20                int need = Math.min(dp[i + 1][j], dp[i][j + 1]);
-21                dp[i][j] = Math.max(1, need - dungeon[i][j]);
-22            }
-23        }
-24
-25        return dp[0][0];
-26    }
-27}
+2    public int maxProfit(int k, int[] prices) {
+3        int n = prices.length;
+4
+5        if (n == 0 || k == 0) {
+6            return 0;
+7        }
+8
+9        if (k >= n / 2) {
+10            int profit = 0;
+11            for (int i = 1; i < n; i++) {
+12                if (prices[i] > prices[i - 1]) {
+13                    profit += prices[i] - prices[i - 1];
+14                }
+15            }
+16            return profit;
+17        }
+18
+19        int[][] dp = new int[k + 1][n];
+20
+21        for (int t = 1; t <= k; t++) {
+22            int maxDiff = -prices[0];
+23            for (int i = 1; i < n; i++) {
+24                dp[t][i] = Math.max(dp[t][i - 1], prices[i] + maxDiff);
+25                maxDiff = Math.max(maxDiff, dp[t - 1][i] - prices[i]);
+26            }
+27        }
+28
+29        return dp[k][n - 1];
+30    }
+31}
