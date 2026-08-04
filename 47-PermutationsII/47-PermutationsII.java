8292379b2e4c18f077f76b0648bf5d26b1411e7c
Kamalika <kamalikarameshkumar@gmail.com>
2026-08-04 09:12:36 +0530

    File updated at 8/4/2026, 9:12:35 AM

--- 47-PermutationsII/47-PermutationsII.java
+++ 47-PermutationsII/47-PermutationsII.java
@@ -1,24 +1,19 @@
-// Last updated: 8/4/2026, 9:11:23 AM
+// Last updated: 8/4/2026, 9:12:35 AM
 1class Solution {
-2    public void rotate(int[][] matrix) {
-3        int n = matrix.length;
-4        for (int i = 0; i < n; i++) {
-5            for (int j = i; j < n; j++) {
-6                int temp = matrix[i][j];
-7                matrix[i][j] = matrix[j][i];
-8                matrix[j][i] = temp;
-9            }
-10        }
-11        for (int i = 0; i < n; i++) {
-12            int left = 0;
-13            int right = n - 1;
-14            while (left < right) {
-15                int temp = matrix[i][left];
-16                matrix[i][left] = matrix[i][right];
-17                matrix[i][right] = temp;
-18                left++;
-19                right--;
-20            }
-21        }
-22    }
-23}
+2    public double myPow(double x, int n) {
+3        long power = n;
+4        if (power < 0) {
+5            x = 1 / x;
+6            power = -power;
+7        }
+8        double result = 1.0;
+9        while (power > 0) {
+10            if (power % 2 == 1) {
+11                result = result * x;
+12            }
+13            x = x * x;
+14            power = power / 2;
+15        }
+16        return result;
+17    }
+18}
