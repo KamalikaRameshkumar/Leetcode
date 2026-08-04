50d15f4d523b5ac5e8ab21bb9966e782fcfb4029
Kamalika <kamalikarameshkumar@gmail.com>
2026-08-04 09:15:15 +0530

    File updated at 8/4/2026, 9:15:14 AM

--- 47-PermutationsII/47-PermutationsII.java
+++ 47-PermutationsII/47-PermutationsII.java
@@ -1,22 +1,22 @@
-// Last updated: 8/4/2026, 9:14:11 AM
+// Last updated: 8/4/2026, 9:15:14 AM
 1class Solution {
-2    public int[][] merge(int[][] intervals) {
-3        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
-4        List<int[]> result = new ArrayList<>();
-5        int start = intervals[0][0];
-6        int end = intervals[0][1];
-7        for (int i = 1; i < intervals.length; i++) {
-8            if (intervals[i][0] <= end) {
-9                end = Math.max(end, intervals[i][1]);
-10            } 
-11            else {
-12                result.add(new int[]{start, end});
-13                start = intervals[i][0];
-14                end = intervals[i][1];
-15            }
-16        }
-17        result.add(new int[]{start, end});
-18
+2    public int[][] insert(int[][] intervals, int[] newInterval) {
+3        List<int[]> result = new ArrayList<>();
+4        int i = 0;
+5        while (i < intervals.length && intervals[i][1] < newInterval[0]) {
+6            result.add(intervals[i]);
+7            i++;
+8        }
+9        while (i < intervals.length && intervals[i][0] <= newInterval[1]) {
+10            newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
+11            newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+12            i++;
+13        }
+14        result.add(newInterval);
+15        while (i < intervals.length) {
+16            result.add(intervals[i]);
+17            i++;
+18        }
 19        return result.toArray(new int[result.size()][]);
 20    }
 21}
