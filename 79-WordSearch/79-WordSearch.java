05be12aa534b780c0f11de9292fb02fba92f4c42
Kamalika <kamalikarameshkumar@gmail.com>
2026-08-04 09:26:46 +0530

    File updated at 8/4/2026, 9:26:46 AM

--- 79-WordSearch/79-WordSearch.java
+++ 79-WordSearch/79-WordSearch.java
@@ -1,4 +1,4 @@
-// Last updated: 8/4/2026, 9:24:37 AM
+// Last updated: 8/4/2026, 9:26:46 AM
 1/**
 2 * Definition for singly-linked list.
 3 * public class ListNode {
@@ -9,24 +9,24 @@
 8 *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
 9 * }
 10 */
-11 class Solution {
-12    public ListNode deleteDuplicates(ListNode head) {
-13        ListNode dummy = new ListNode(0);
-14        dummy.next = head;
-15        ListNode prev = dummy;
-16        ListNode curr = head;
-17        while (curr != null) {
-18            if (curr.next != null && curr.val == curr.next.val) {
-19                int duplicate = curr.val;
-20                while (curr != null && curr.val == duplicate) {
-21                    curr = curr.next;
-22                }
-23                prev.next = curr;
-24            } else {
-25                prev = curr;
-26                curr = curr.next;
-27            }
-28        }
-29        return dummy.next;
+11class Solution {
+12    public ListNode partition(ListNode head, int x) {
+13        ListNode lessDummy = new ListNode(0);
+14        ListNode greaterDummy = new ListNode(0);
+15        ListNode less = lessDummy;
+16        ListNode greater = greaterDummy;
+17        while (head != null) {
+18            if (head.val < x) {
+19                less.next = head;
+20                less = less.next;
+21            } else {
+22                greater.next = head;
+23                greater = greater.next;
+24            }
+25            head = head.next;
+26        }
+27        greater.next = null;
+28        less.next = greaterDummy.next;
+29        return lessDummy.next;
 30    }
 31}
