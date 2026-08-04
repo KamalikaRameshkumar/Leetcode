6eea287e57efba451db50d9188ac1962152bd635
Kamalika <kamalikarameshkumar@gmail.com>
2026-08-04 09:27:41 +0530

    File updated at 8/4/2026, 9:27:40 AM

--- 79-WordSearch/79-WordSearch.java
+++ 79-WordSearch/79-WordSearch.java
@@ -1,32 +1,11 @@
-// Last updated: 8/4/2026, 9:26:46 AM
-1/**
-2 * Definition for singly-linked list.
-3 * public class ListNode {
-4 *     int val;
-5 *     ListNode next;
-6 *     ListNode() {}
-7 *     ListNode(int val) { this.val = val; }
-8 *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-9 * }
-10 */
-11class Solution {
-12    public ListNode partition(ListNode head, int x) {
-13        ListNode lessDummy = new ListNode(0);
-14        ListNode greaterDummy = new ListNode(0);
-15        ListNode less = lessDummy;
-16        ListNode greater = greaterDummy;
-17        while (head != null) {
-18            if (head.val < x) {
-19                less.next = head;
-20                less = less.next;
-21            } else {
-22                greater.next = head;
-23                greater = greater.next;
-24            }
-25            head = head.next;
-26        }
-27        greater.next = null;
-28        less.next = greaterDummy.next;
-29        return lessDummy.next;
-30    }
-31}
+// Last updated: 8/4/2026, 9:27:40 AM
+1class Solution {
+2    public List<Integer> grayCode(int n) {
+3        List<Integer> result = new ArrayList<>();
+4        int size = 1 << n;
+5        for (int i = 0; i < size; i++) {
+6            result.add(i ^ (i >> 1));
+7        }
+8        return result;
+9    }
+10}
