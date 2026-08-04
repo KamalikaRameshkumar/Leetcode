6db6349ac362f548c0e218114147485f27dd206d
Kamalika <kamalikarameshkumar@gmail.com>
2026-08-04 09:14:12 +0530

    File updated at 8/4/2026, 9:14:11 AM

--- 47-PermutationsII/47-PermutationsII.java
+++ 47-PermutationsII/47-PermutationsII.java
@@ -1,16 +1,22 @@
-// Last updated: 8/4/2026, 9:13:16 AM
+// Last updated: 8/4/2026, 9:14:11 AM
 1class Solution {
-2    public boolean canJump(int[] nums) {
-3        int farthest = 0;
-4        for (int i = 0; i < nums.length; i++) {
-5            if (i > farthest) {
-6                return false;
-7            }
-8            farthest = Math.max(farthest, i + nums[i]);
-9            if (farthest >= nums.length - 1) {
-10                return true;
-11            }
-12        }
-13        return true;
-14    }
-15}
+2    public int[][] merge(int[][] intervals) {
+3        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+4        List<int[]> result = new ArrayList<>();
+5        int start = intervals[0][0];
+6        int end = intervals[0][1];
+7        for (int i = 1; i < intervals.length; i++) {
+8            if (intervals[i][0] <= end) {
+9                end = Math.max(end, intervals[i][1]);
+10            } 
+11            else {
+12                result.add(new int[]{start, end});
+13                start = intervals[i][0];
+14                end = intervals[i][1];
+15            }
+16        }
+17        result.add(new int[]{start, end});
+18
+19        return result.toArray(new int[result.size()][]);
+20    }
+21}
