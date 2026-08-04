a5e7ad8ae690042ab9d30ccb688755440c493164
Kamalika <kamalikarameshkumar@gmail.com>
2026-08-04 09:47:38 +0530

    File updated at 8/4/2026, 9:47:37 AM

--- 79-WordSearch/79-WordSearch.java
+++ 79-WordSearch/79-WordSearch.java
@@ -1,33 +1,17 @@
-// Last updated: 8/4/2026, 9:46:28 AM
-1/**
-2 * Definition for a binary tree node.
-3 * public class TreeNode {
-4 *     int val;
-5 *     TreeNode left;
-6 *     TreeNode right;
-7 *     TreeNode() {}
-8 *     TreeNode(int val) { this.val = val; }
-9 *     TreeNode(int val, TreeNode left, TreeNode right) {
-10 *         this.val = val;
-11 *         this.left = left;
-12 *         this.right = right;
-13 *     }
-14 * }
-15 */
-16class Solution {
-17    public void flatten(TreeNode root) {
-18        if (root == null) {
-19            return;
-20        }
-21        flatten(root.left);
-22        flatten(root.right);
-23        TreeNode rightSubtree = root.right;
-24        root.right = root.left;
-25        root.left = null;
-26        TreeNode current = root;
-27        while (current.right != null) {
-28            current = current.right;
-29        }
-30        current.right = rightSubtree;
-31    }
-32}
+// Last updated: 8/4/2026, 9:47:37 AM
+1class Solution {
+2    public int minimumTotal(List<List<Integer>> triangle) {
+3        int n = triangle.size();
+4        int[] dp = new int[n];
+5        for (int i = 0; i < n; i++) {
+6            dp[i] = triangle.get(n - 1).get(i);
+7        }
+8        for (int i = n - 2; i >= 0; i--) {
+9            for (int j = 0; j <= i; j++) {
+10                dp[j] = triangle.get(i).get(j)
+11                       + Math.min(dp[j], dp[j + 1]);
+12            }
+13        }
+14        return dp[0];
+15    }
+16}
