962ceb360f708e00de809d32de76b6377cce52ef
Kamalika <kamalikarameshkumar@gmail.com>
2026-08-04 09:24:38 +0530

    File updated at 8/4/2026, 9:24:37 AM

--- 79-WordSearch/79-WordSearch.java
+++ 79-WordSearch/79-WordSearch.java
@@ -1,16 +1,32 @@
-// Last updated: 8/4/2026, 9:23:17 AM
-1class Solution {
-2    public int removeDuplicates(int[] nums) {
-3        if (nums.length <= 2) {
-4            return nums.length;
-5        }
-6        int k = 2;
-7        for (int i = 2; i < nums.length; i++) {
-8            if (nums[i] != nums[k - 2]) {
-9                nums[k] = nums[i];
-10                k++;
-11            }
-12        }
-13        return k;
-14    }
-15}
+// Last updated: 8/4/2026, 9:24:37 AM
+1/**
+2 * Definition for singly-linked list.
+3 * public class ListNode {
+4 *     int val;
+5 *     ListNode next;
+6 *     ListNode() {}
+7 *     ListNode(int val) { this.val = val; }
+8 *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+9 * }
+10 */
+11 class Solution {
+12    public ListNode deleteDuplicates(ListNode head) {
+13        ListNode dummy = new ListNode(0);
+14        dummy.next = head;
+15        ListNode prev = dummy;
+16        ListNode curr = head;
+17        while (curr != null) {
+18            if (curr.next != null && curr.val == curr.next.val) {
+19                int duplicate = curr.val;
+20                while (curr != null && curr.val == duplicate) {
+21                    curr = curr.next;
+22                }
+23                prev.next = curr;
+24            } else {
+25                prev = curr;
+26                curr = curr.next;
+27            }
+28        }
+29        return dummy.next;
+30    }
+31}
