acaa0230cdc8d14882a0841329d0061c41e79972
Kamalika <kamalikarameshkumar@gmail.com>
2026-08-04 09:11:24 +0530

    File updated at 8/4/2026, 9:11:23 AM

--- 47-PermutationsII/47-PermutationsII.java
+++ 47-PermutationsII/47-PermutationsII.java
@@ -1,31 +1,24 @@
-// Last updated: 8/4/2026, 9:10:09 AM
+// Last updated: 8/4/2026, 9:11:23 AM
 1class Solution {
-2    public List<List<Integer>> permuteUnique(int[] nums) {
-3        List<List<Integer>> result = new ArrayList<>();
-4        Arrays.sort(nums);
-5        boolean[] used = new boolean[nums.length];
-6        backtrack(nums, used, new ArrayList<>(), result);
-7        return result;
-8    }
-9    private void backtrack(int[] nums, boolean[] used,
-10                           List<Integer> current,
-11                           List<List<Integer>> result) {
-12        if (current.size() == nums.length) {
-13            result.add(new ArrayList<>(current));
-14            return;
-15        }
-16        for (int i = 0; i < nums.length; i++) {
-17            if (used[i]) {
-18                continue;
-19            }
-20            if (i > 0 && nums[i] == nums[i - 1] && !used[i - 1]) {
-21                continue;
-22            }
-23            used[i] = true;
-24            current.add(nums[i]);
-25            backtrack(nums, used, current, result);
-26            current.remove(current.size() - 1);
-27            used[i] = false;
-28        }
-29    }
-30}
+2    public void rotate(int[][] matrix) {
+3        int n = matrix.length;
+4        for (int i = 0; i < n; i++) {
+5            for (int j = i; j < n; j++) {
+6                int temp = matrix[i][j];
+7                matrix[i][j] = matrix[j][i];
+8                matrix[j][i] = temp;
+9            }
+10        }
+11        for (int i = 0; i < n; i++) {
+12            int left = 0;
+13            int right = n - 1;
+14            while (left < right) {
+15                int temp = matrix[i][left];
+16                matrix[i][left] = matrix[i][right];
+17                matrix[i][right] = temp;
+18                left++;
+19                right--;
+20            }
+21        }
+22    }
+23}
