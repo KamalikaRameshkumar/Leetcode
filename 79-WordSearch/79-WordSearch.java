e66d04a3db13d4ab8fe458ad1582ebf7973fd599
Kamalika <kamalikarameshkumar@gmail.com>
2026-08-04 09:46:29 +0530

    File updated at 8/4/2026, 9:46:28 AM

--- 79-WordSearch/79-WordSearch.java
+++ 79-WordSearch/79-WordSearch.java
@@ -1,4 +1,4 @@
-// Last updated: 8/4/2026, 9:43:55 AM
+// Last updated: 8/4/2026, 9:46:28 AM
 1/**
 2 * Definition for a binary tree node.
 3 * public class TreeNode {
@@ -15,24 +15,19 @@
 14 * }
 15 */
 16class Solution {
-17    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
-18        List<List<Integer>> result = new ArrayList<>();
-19        dfs(root, targetSum, new ArrayList<>(), result);
-20        return result;
-21    }
-22    private void dfs(TreeNode root, int targetSum,List<Integer> path,List<List<Integer>> result) {
-23        if (root == null) {
-24            return;
-25        }
-26        path.add(root.val);
-27        if (root.left == null && root.right == null) {
-28            if (targetSum == root.val) {
-29                result.add(new ArrayList<>(path));
-30            }
-31        } else {
-32            dfs(root.left, targetSum - root.val, path, result);
-33            dfs(root.right, targetSum - root.val, path, result);
-34        }
-35        path.remove(path.size() - 1);
-36    }
-37}
+17    public void flatten(TreeNode root) {
+18        if (root == null) {
+19            return;
+20        }
+21        flatten(root.left);
+22        flatten(root.right);
+23        TreeNode rightSubtree = root.right;
+24        root.right = root.left;
+25        root.left = null;
+26        TreeNode current = root;
+27        while (current.right != null) {
+28            current = current.right;
+29        }
+30        current.right = rightSubtree;
+31    }
+32}
