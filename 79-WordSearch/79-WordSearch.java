4a8ca0f56f2b11c286ab52027a7c5d91f09192fe
Kamalika <kamalikarameshkumar@gmail.com>
2026-08-04 09:49:57 +0530

    File updated at 8/4/2026, 9:49:56 AM

--- 79-WordSearch/79-WordSearch.java
+++ 79-WordSearch/79-WordSearch.java
@@ -1,12 +1,22 @@
-// Last updated: 8/4/2026, 9:48:24 AM
+// Last updated: 8/4/2026, 9:49:56 AM
 1class Solution {
-2    public int maxProfit(int[] prices) {
-3        int profit = 0;
-4        for (int i = 1; i < prices.length; i++) {
-5            if (prices[i] > prices[i - 1]) {
-6                profit += prices[i] - prices[i - 1];
-7            }
-8        }
-9        return profit;
-10    }
-11}
+2    public int longestConsecutive(int[] nums) {
+3        Set<Integer> set = new HashSet<>();
+4        for (int num : nums) {
+5            set.add(num);
+6        }
+7        int longest = 0;
+8        for (int num : set) {
+9            if (!set.contains(num - 1)) {
+10                int current = num;
+11                int length = 1;
+12                while (set.contains(current + 1)) {
+13                    current++;
+14                    length++;
+15                }
+16                longest = Math.max(longest, length);
+17            }
+18        }
+19        return longest;
+20    }
+21}
