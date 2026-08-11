b42885b122fc38b11ef2d64a90a82b321e316de8
Kamalika <kamalikarameshkumar@gmail.com>
2026-08-11 09:35:53 +0530

    File updated at 8/11/2026, 9:35:53 AM

--- 139-WordBreak/139-WordBreak.java
+++ 139-WordBreak/139-WordBreak.java
@@ -1,47 +1,35 @@
-// Last updated: 8/11/2026, 9:34:30 AM
+// Last updated: 8/11/2026, 9:35:53 AM
 1/**
-2 * Definition for singly-linked list.
-3 * public class ListNode {
+2 * Definition for a binary tree node.
+3 * public class TreeNode {
 4 *     int val;
-5 *     ListNode next;
-6 *     ListNode() {}
-7 *     ListNode(int val) { this.val = val; }
-8 *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-9 * }
-10 */
-11class Solution {
-12    public void reorderList(ListNode head) {
-13
-14        if (head == null || head.next == null) {
-15            return;
-16        }
-17        ListNode slow = head;
-18        ListNode fast = head;
+5 *     TreeNode left;
+6 *     TreeNode right;
+7 *     TreeNode() {}
+8 *     TreeNode(int val) { this.val = val; }
+9 *     TreeNode(int val, TreeNode left, TreeNode right) {
+10 *         this.val = val;
+11 *         this.left = left;
+12 *         this.right = right;
+13 *     }
+14 * }
+15 */
+16class Solution {
+17    public List<Integer> postorderTraversal(TreeNode root) {
+18        List<Integer> result = new ArrayList<>();
 19
-20        while (fast != null && fast.next != null) {
-21            slow = slow.next;
-22            fast = fast.next.next;
-23        }
-24        ListNode prev = null;
-25        ListNode curr = slow;
-26
-27        while (curr != null) {
-28            ListNode next = curr.next;
-29            curr.next = prev;
-30            prev = curr;
-31            curr = next;
-32        }
-33        ListNode first = head;
-34        ListNode second = prev;
-35        while (second.next != null) {
-36            ListNode temp1 = first.next;
-37            ListNode temp2 = second.next;
-38
-39            first.next = second;
-40            second.next = temp1;
-41
-42            first = temp1;
-43            second = temp2;
-44        }
-45    }
-46}
+20        postorder(root, result);
+21
+22        return result;
+23    }
+24
+25    public void postorder(TreeNode root, List<Integer> result) {
+26        if (root == null) {
+27            return;
+28        }
+29
+30        postorder(root.left, result);
+31        postorder(root.right, result);
+32        result.add(root.val);
+33    }
+34}
